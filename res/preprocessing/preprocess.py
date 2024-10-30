8237b561e713d8f8d7ab4b1ef46e6070557537e2
Gabriele Dellepere <gabriele.dellepere@nextage-on.com>
2024-10-30 09:32:50 +0100

    added 10 years chart

--- res/preprocessing/preprocess.py
+++ res/preprocessing/preprocess.py
@@ -13,25 +13,15 @@ df_cleaned = df.dropna(subset=[colname])
 df_useful = df_cleaned[df_cleaned['Year'] >= 2012]
 df_useful = df_useful[df_useful['Year'] <= 2022]
 
-df_mean = df_useful.groupby('Entity')[colname].mean()
-
 df_2022 = df_cleaned[df_cleaned['Year'] == 2022]
-
-
-# Salva il DataFrame come CSV
-#df2022.to_csv(percorso, index=False)
-
-# Controlla i valori mancanti nella colonna 'variabile'
-
-
-
-
-
 df_sorted_2022 = df_2022.sort_values(by=[colname], ascending=False)
 
+df_mean = df_useful.groupby('Entity')[colname].mean().reset_index()
+df_sorted_mean = df_mean.sort_values(by=[colname], ascending=False)
+
 percorso = '../../src/data/co2_2022.csv'
-per2 = '../../src/data/co2_2022_mean.csv'
+percorso_medie = '../../src/data/co2_2022_mean.csv'
 
 # Salva il DataFrame come CSV
 df_sorted_2022.to_csv(percorso, index=False)
-df_mean.to_csv(per2, index=False )
+df_sorted_mean.to_csv(percorso_medie, index=False )
